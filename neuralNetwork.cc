717de6cb7653e1a585f00089bc01525813c11345
sahil <svishwajeet750@gmail.com>
2026-08-27 00:49:51 +0530

    two layer

--- neuralNetwork.cc
+++ neuralNetwork.cc
@@ -4,11 +4,14 @@
 #include <vector>
 using namespace std;
 
-int countNeurals = 2;
 // how much sleep , how good is weather  
-vector<vector<double>> weights{{.7, .8}, {.3, .4}};
+vector<vector<double>> l1weights{{.7, .8}, {.3, .4}};
 // generally dont wanna run 
-vector<double> biases {-1, 3};
+vector<double> l1biases {-1, 3};
+
+vector<vector<double>> l2weights{ {.2, .1}};
+// generally dont wanna run 
+vector<double> l2biases {2};
 
 
 vector<double> inputs{.5, .9};
@@ -20,11 +23,11 @@ auto relu ( double outputWithBias) {
 
 vector<double> neuralNetLayer(vector<double> inputs, vector<vector<double>> weights, vector<double> biases) {
     //
-    int count = weights.size();
+    int countNeurons = weights.size();
 
     vector<double> output = biases; 
 
-    for(int i = 0; i < count; i ++)
+    for(int i = 0; i < countNeurons; i ++)
     {
         for (int  j =0 ; j < inputs.size(); j ++)
             output[i] += weights[i][j] * inputs[j]; 
@@ -36,8 +39,13 @@ vector<double> neuralNetLayer(vector<double> inputs, vector<vector<double>> weig
 }
 
 int main () {
-    auto ans =  neuralNetLayer(inputs, weights, biases);
-    for(auto i  :ans) cout << i << " ";
+    
+
+    auto l1ans =  neuralNetLayer(inputs, l1weights, l1biases);
+    auto l2ans = neuralNetLayer(l1ans, l2weights , l2biases);
+
+    
+    for(auto i : l2ans) cout << i << " ";
     cout << endl;
 
 }
